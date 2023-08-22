4d2823ec7aab38a0f419c4f508f5c2b7ead2b075
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-08-22 08:33:57 +0600

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ const text=document.getElementById("quote");
 const author=document.getElementById("author");
 
 const getNewQuote = async () =>
+
+
+//api for quotes
+
+    var url="https://type.fit/api/quotes";    
