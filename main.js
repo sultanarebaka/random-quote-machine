a90ee4c5202a78529a889774417451cda1745098
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-08-31 19:30:50 +0600

    Update main.js

--- main.js
+++ main.js
@@ -5,49 +5,32 @@ const text=document.getElementById("quote");
 
 const author=document.getElementById("author");
 
-
-
-
 const getNewQuote = async () =>
 
 {
-
     const response=await fetch(url);
 
     console.log(typeoresponsese);
-
     const quote=allQuotes[indx].text;
 
-   
-
 const response=await fetch(url);
-
-
-
 const quote=allQuotes[indx].text;
 
     
+    // json and store it in quotes array
 
-const allQuotes = await response.json();
-
-
+    const allQuotes = await response.json();
 
 // json and store it in quotes array
 
     const allQuotes =
 
-
 const auth=allQuotes[indx].author;
-
 const indx = Math.floor(Math.random()*allQuotes.length);
-
 
 genarate a random number
 
-
-
 // display the quote and the author
-
     text.innerHTML=quote;
 
     author.innerHTML="~ "+auth;
